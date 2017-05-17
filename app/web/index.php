ccf2f28f5d0c6a1d4668b7b63b844e044fa4d46b
Jon Stoddart <jstoddart@avantlink.com>
2017-05-17 11:22:02 -0600

    Temporarily switched from Doctrine.

--- app/web/index.php
+++ app/web/index.php
@@ -2,6 +2,19 @@
 
 require_once '../config.php';
 
-$waits = $entityManager->getRepository('RideWait')->getLatestWaits();
+$db = new PDO("mysql:host={$config['database']['host']};port={$config['database']['port']};dbname={$config['database']['database']}", $config['database']['username'], $config['database']['password']);
+
+$sql = '
+        SELECT RW.*
+        FROM ride_waits RW
+        INNER JOIN (
+            SELECT ride_name,
+            MAX(ride_wait_id) AS max_id
+            FROM ride_waits
+            GROUP BY ride_name
+        ) RW_ID ON RW.ride_wait_id = RW_ID.max_id
+    ';
+$waits = $db->query($sql);
+//$waits = $entityManager->getRepository('RideWait')->getLatestWaits();
 
 echo $twig->render('pages/home.html.twig', ['waits' => $waits]);
