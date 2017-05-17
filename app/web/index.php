1843a12e755e6e4258b4e5cea21ea8078280240d
Jon Stoddart <jstoddart@avantlink.com>
2017-05-16 23:56:41 -0600

    Added use.

--- app/web/index.php
+++ app/web/index.php
@@ -2,6 +2,8 @@
 
 require_once '../config.php';
 
+use RideWait;
+
 $waits = $entityManager->getRepository('RideWait')->getLatestWaits();
 
 echo $twig->render('pages/home.html.twig', ['waits' => $waits]);
